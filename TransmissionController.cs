ddf301a8dc8677bca79662a77205e00940fe479c
Scott Dexter <scottgdexter@gmail.com>
2013-11-14 23:13:16 -0700

    init

---
+++ TransmissionController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ScannerPOC.Models;
+
+namespace ScannerPOC.Controllers
+{
+    public class TransmissionController : Controller
+    {
+        private Entities db = new Entities();
+
+        //
+        // GET: /Transmission/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        //
+        // GET: /Transmission/Nearby/5
+        // return transmissions after (2 hrs) and a little before (30 min?) this one
+        public ActionResult Nearby(Transmission thistrans)
+        {
+            thistrans = db.Transmissions.Find(thistrans.Id);
+            DateTime justbefore = thistrans.TimeStamp.AddMinutes(-30);
+            DateTime justafter = thistrans.TimeStamp.AddHours(2);
+            List<Transmission> nearby =
+                db.Transmissions.AsQueryable().Where(
+                    x => (x.TimeStamp>=justbefore)&&(x.TimeStamp<=justafter)&&(x.ChannelName == thistrans.ChannelName) && (x.ChannelSubName == thistrans.ChannelSubName)).OrderBy(x=>x.TimeStamp).ToList();
+            return new JsonResult(){Data=nearby, JsonRequestBehavior = JsonRequestBehavior.AllowGet};
+        }
+
+    }
+}
---
+++ quick_blog/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+QuickBlog::Application.config.secret_key_base = 'c1396c102d65ddd677a50badffe0b22ca352414c72fcbf28cb397a1a2334e900a118ae663475cbd79b799140a3a99dfca85498e795a99344234cca13a3bcb94b'
